43027f69cc5ab662e469b72cbd52d1f69c4a9b0f
Krish Nagaral <nagaralkrish@gmail.com>
2026-09-21 16:32:29 -0400

    Add name to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <p className="text-lg font-medium text-black dark:text-zinc-50">
+          Krish Nagaral
+        </p>
         <Image
           className="dark:invert h-5 w-[100px]"
           src="/next.svg"
