4939a1c69ad5c3a22afda8fa345a24681dc59c22
Krish Nagaral <nagaralkrish@gmail.com>
2026-09-25 18:18:11 -0400

    Add missing link ids to Labs TOC

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -10,22 +10,22 @@ export default function TOC() {
       </div>
       <ul>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" id="wd-home-link">Home</Link>
         </li>
         <li>
-          <Link href="/labs/lab1">Lab 1</Link>
+          <Link href="/labs/lab1" id="wd-lab1-link">Lab 1</Link>
         </li>
         <li>
-          <Link href="/labs/lab2">Lab 2</Link>
+          <Link href="/labs/lab2" id="wd-lab2-link">Lab 2</Link>
         </li>
         <li>
-          <Link href="/labs/lab3">Lab 3</Link>
+          <Link href="/labs/lab3" id="wd-lab3-link">Lab 3</Link>
         </li>
         <li>
-          <Link href="/labs/lab4">Lab 4</Link>
+          <Link href="/labs/lab4" id="wd-lab4-link">Lab 4</Link>
         </li>
         <li>
-          <Link href="/labs/lab5">Lab 5</Link>
+          <Link href="/labs/lab5" id="wd-lab5-link">Lab 5</Link>
         </li>
         <li>
           <Link href="/" id="wd-kambaz-link">
